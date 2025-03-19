d845c24d6edd9d23ab95d285fba1807070a1764d
JoseJimenez20w <141762910+josejimenez20@users.noreply.github.com>
2025-03-19 00:12:58 -0600

    Cambios

--- main.js
+++ main.js
@@ -3,6 +3,25 @@ const {v4: uuidv4} = uuid;
 const Dexie = window.Dexie,
     db = new Dexie('db_academico');
 
+
+    window.addEventListener('load', () => {
+        if (navigator.onLine) {
+            console.log("Conectado a Internet");
+            sincronizarDatos(); 
+        } else {
+            console.log("Sin conexión, guardando en IndexedDB");
+        }
+    });
+
+    window.addEventListener('online', () => {
+        console.log("Se ha reconectado, sincronizando datos...");
+        sincronizarDatos();
+    });
+    
+    window.addEventListener('offline', () => {
+        console.log("Se ha perdido la conexión, guardando en IndexedDB...");
+    });
+
 const app = createApp({
     components: {
         alumno,
